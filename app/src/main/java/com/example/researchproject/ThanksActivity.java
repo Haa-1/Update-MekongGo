7d9a37ef65c5855f699adc0d6286c6c081188594
Haa-1 <havtbs00551@fpt.edu.vn>
2025-04-12 00:21:34 +0700

    layout thank you

---
+++ app/src/main/java/com/example/researchproject/ThanksActivity.java
@@ -0,0 +1,12 @@
+package com.example.researchproject;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThanksActivity extends AppCompatActivity {
+
+}
